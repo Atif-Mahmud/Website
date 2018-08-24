2363efc7d03ae22a2e507d816b46195081e602e7
Atif Mahmud <atifmahmud101@gmail.com>
2018-08-24 09:54:56 -0400

    Generate LayoutComponent scaffold and import

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavigationComponent]
+  declarations: [NavigationComponent, LayoutComponent]
 })
 export class UiModule { }
