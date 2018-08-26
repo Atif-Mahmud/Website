3f5f6ff8916c09cbf67b364c7f49fefcf16b32d8
Atif Mahmud <atifmahmud101@gmail.com>
2018-08-26 12:31:22 -0400

    Tweak content

--- src/app/ui/banner/banner.component.ts
+++ src/app/ui/banner/banner.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
+  announcement: String = 'Placeholder announcement';
 
   constructor() { }
 
