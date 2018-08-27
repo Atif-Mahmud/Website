bd6cf6fb6ad8f42d4a82e05ea0cb3ea03b1ee274
Atif Mahmud <atifmahmud101@gmail.com>
2018-08-27 10:53:27 -0400

    Add HomeComponent to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PagesModule } from './pages/pages.module';
+import { HomeComponent } from './pages/home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PagesModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   declarations: []
 })
